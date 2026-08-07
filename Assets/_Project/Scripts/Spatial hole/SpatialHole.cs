9c88e773a6b2087723e5ad28d4987dc103bd4ac1
Antos_Codes <temp88665@gmail.com>
2026-08-08 00:26:30 +0300

    Spatial Holes
    Fix

--- Assets/_Project/Scripts/Spatial hole/SpatialHole.cs
+++ Assets/_Project/Scripts/Spatial hole/SpatialHole.cs
@@ -9,6 +9,7 @@ namespace Game.SpatialHole
         [SerializeField] private Camera camera;
         [SerializeField] private MeshRenderer meshRenderer;
         [SerializeField] private Shader shader;
+        [SerializeField] private LayerMask mask;
         [SerializeField] private int size = 2048;
         [SerializeField] private int depth = 24;
         [SerializeField] private bool isTeleport = false;
@@ -24,26 +25,33 @@ namespace Game.SpatialHole
         {
             this.spatialHole = spatialHole;
             this.spatialHole.Camera.targetTexture = new(size, size, depth);
-            meshRenderer.materials = new Material[]{(new (shader))};
+            meshRenderer.materials = new Material[] { (new(shader)) };
             meshRenderer.sharedMaterial.mainTexture = spatialHole.Camera.targetTexture;
             Show();
         }
 
         private void Update()
         {
-            Quaternion difference = transform.rotation * Quaternion.Inverse(spatialHole.transform.rotation * Quaternion.Euler(0,180,0));
-            camera.transform.rotation = difference * Camera.main.transform.rotation;
-        }
+            var colliders = Physics.OverlapBox(transform.position, transform.localScale,transform.rotation, mask);
 
-        private void OnCollisionEnter(Collision collision)
-        {
-            if (!isTeleport || !collision.gameObject.TryGetComponent(out Rigidbody rb)) return;
-            
-            var magnitude = rb.velocity.magnitude;
-            rb.velocity = Vector3.zero;
-            rb.transform.position = spatialHole.transform.position;
-            rb.transform.localScale = Vector3.one * 5f;
-            rb.AddForce(spatialHole.transform.forward * magnitude, ForceMode.Impulse);
+            if (colliders != null && colliders.Length > 0)
+            {
+                foreach (var collider in colliders)
+                {
+                    if (!isTeleport || !collider.gameObject.TryGetComponent(out Rigidbody rb)) return;
+
+                    var magnitude = rb.velocity.magnitude;
+                    rb.velocity = Vector3.zero;
+                    rb.position = spatialHole.transform.position;
+                    rb.transform.localScale = Vector3.one * 5f;
+                    var direction = spatialHole.transform.TransformDirection(Vector3.right) - transform.TransformDirection(Vector3.left);
+                    rb.AddForce(direction * magnitude, ForceMode.Impulse);
+                }
+            }
+
+            Quaternion difference = transform.rotation *
+                                    Quaternion.Inverse(spatialHole.transform.rotation * Quaternion.Euler(0, 180, 0));
+            camera.transform.rotation = difference * Camera.main.transform.rotation;
         }
     }
 }
